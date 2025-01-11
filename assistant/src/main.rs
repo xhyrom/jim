d119fefea35dcb193ea8eb145f712f97093e89c9
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2025-01-11 20:10:16 +0100

    feat: assistant

--- assistant/src/main.rs
+++ assistant/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("hello");
+use stt::config;
+
+#[tokio::main]
+async fn main() {
+    let model_path = std::env::args()
+        .nth(1)
+        .expect("Please specify path to model");
+    let wav_path = std::env::args()
+        .nth(2)
+        .expect("Please specify path to wav file");
+
+    let provider = stt::create_provider(&config::SpeechToTextConfig {
+        model_path: Some(model_path),
+        worker_url: None,
+        api_key: None,
+    });
+
+    let wav = std::fs::read(wav_path).expect("Failed to read wav file");
+    let text = provider.transcribe(wav).await.unwrap();
+
+    println!("{}", text);
 }
--- providers/stt/core/src/lib.rs
+++ providers/stt/core/src/lib.rs
@@ -3,14 +3,23 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum TranscriberError {
+    #[error("IO: {0}")]
+    Io(#[from] std::io::Error),
+
     #[error("Invalid audio data")]
     InvalidAudioData,
 
     #[error("Unknown error: {0}")]
     Unknown(String),
+
+    #[error("Wishper error: {0}")]
+    Whisper(String),
+
+    #[error(transparent)]
+    Provider(#[from] anyhow::Error),
 }
 
 #[async_trait]
-pub trait TranscriberProvider {
+pub trait TranscriberProvider: Send + Sync {
     async fn transcribe(&self, audio_data: Vec<u8>) -> Result<String, TranscriberError>;
 }
