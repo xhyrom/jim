f20bb028dd72a101d76427022889ca8b1d4b4bca
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2025-06-01 11:51:39 +0200

    feat(satellite): leds

--- satellite/satellite/leds/drivers/neopixel.py
+++ satellite/satellite/leds/drivers/neopixel.py
@@ -1,5 +1,4 @@
 import logging
-from typing import List, Tuple
 
 from ..controller import LEDController
 
@@ -9,7 +8,7 @@ logger = logging.getLogger(__name__)
 class NeoPixelController(LEDController):
     def __init__(self, pin: int = 18, num_leds: int = 10, brightness: float = 0.5): ...
 
-    def set_pixels(self, colors: List[Tuple[int, int, int]]) -> None: ...
+    def set_pixels(self, colors: list[tuple[int, int, int]]) -> None: ...
 
     def off(self) -> None: ...
 
